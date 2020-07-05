9d317a189b9983fe4cc652df4e2858d5ee3e6949
codeForX <supercheapme@gmail.com>
2020-07-05 12:26:54 -0400

    fixed a bug in resoulution checker

--- downTube.py
+++ downTube.py
@@ -2,11 +2,11 @@ from pytube import YouTube
 link = input('enter a url to download:  ')
 yt = YouTube(link)
 choice = input('enter the resoulution:  ')
-vid = None
-while vid is None:
-    choice = input('resoulution invalid enter again:  ')
+while True:
     vid = yt.streams.filter(res = choice,  progressive = True ).first()
-
+    if vid:
+        break
+    choice = input('resoulution unavailable enter again:  ')
 print('downloading..')
 
 vid.download(r'C:\Users\sruls\OneDrive\Desktop\yt-dl', yt.title)
