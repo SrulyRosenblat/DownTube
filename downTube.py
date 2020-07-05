8be9af4ca743a9df3aa2336141b5849e1b611caf
codeForX <supercheapme@gmail.com>
2020-07-05 11:07:56 -0400

    added a resoulution picker

--- downTube.py
+++ downTube.py
@@ -1,14 +1,14 @@
 from pytube import YouTube
-
-print('pytube loaded')
 link = input('enter a url to download:  ')
-
 yt = YouTube(link)
-vid = yt.streams.filter(res ='360p',  progressive = True ).first()
+choice = input('enter the resoulution:  ')
+vid = None
+while vid is None:
+    choice = input('resoulution invalid enter again:  ')
+    vid = yt.streams.filter(res = choice,  progressive = True ).first()
 
 print('downloading..')
 
-# the title of the video is 'youtube'
 vid.download(r'C:\Users\sruls\OneDrive\Desktop\yt-dl', yt.title)
 
 print('download complete of "' + yt.title + '"')
