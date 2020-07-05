cd52ceafb23d781f71497f3e20dad78bc8685f68
codeForX <supercheapme@gmail.com>
2020-07-05 09:00:04 -0400

    got rid of extra comments

--- downTube.py
+++ downTube.py
@@ -13,10 +13,4 @@ vid.download(r'C:\Users\sruls\OneDrive\Desktop\yt-dl', yt.title)
 
 print('download complete of "' + yt.title + '"')
 
-
-# yt = YouTube('https://www.youtube.com/watch?v=trKjYdBASyQ')
-# al = yt.streams.all()
-# rez = yt.streams.filter(res ='720p',  progressive = True)
-# #res = yt.streams.filter()
-# for stream in rez:
-#     print(stream)
+print('hi')
