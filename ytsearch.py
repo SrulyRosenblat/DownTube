f9a9d200b6cb90bb01bbd7e054a3ec47e2fe517a
sruly <srulsterer@gmail.com>
2020-08-12 15:38:02 -0400

    made the playlist object itterable and fixed a bug with private youtube videos

--- ytsearch.py
+++ ytsearch.py
@@ -4,11 +4,11 @@ import key
 
 def main():
     YT = youtubeConnect(key.key)
-    playlists = YT.playlist_search('never ending story') 
+    playlists = YT.playlist_search('minecraft',limit=4,maxVidsInPL=50) 
     for playlist in playlists:
         print(playlist.title)
-        for vid in playlist.videos:
-            print(vid.title)
+        print(playlist.videoCount)
+        
         
 class videoobject():
     '''
@@ -43,9 +43,9 @@ class videoobject():
 
 class playlistobject():
     '''
-    takes a youtube api playlist id object as a input and outputs cleaned up data
+    takes a youtube api playlist object as a input and outputs cleaned up data
     '''
-    def __init__(self,playlist,thumbnailQuality = 'medium'):
+    def __init__(self,playlist,thumbnailQuality = 'medium',maxVideos=50):
         yt = youtubeConnect(key.key)
         self.id = playlist['id']['playlistId']
         playlistInfo = playlist['snippet']
@@ -64,16 +64,20 @@ class playlistobject():
             self.thumbnailWidth = thumbnail['default']['width']
             self.thumbnailHeight = thumbnail['default']['height']
         self.videos = []
-        request = yt.api.playlistItems().list(part="snippet",maxResults=30,playlistId=self.id)
+        request = yt.api.playlistItems().list(part="snippet",maxResults=maxVideos,playlistId=self.id)
         self.videoCount = 0
         for vidInfo in request.execute()['items']:
             video = yt.video_by_url(vidInfo['snippet']['resourceId']['videoId'])
-            self.videos.append(video) 
-            self.videoCount += 1
+            if video:
+                self.videos.append(video) 
+                self.videoCount += 1
 
-        
+    def __iter__(self):
+        return (video for video in self.videos)
     def __str__(self):
         return f'title: {self.title},\nid: {self.id}\ndescription: {self.description},\ndate: {self.date},\nchannel: {self.channel},\nthumbnailLink: {self.thumbnailLink},\nthumbnailWidth: {self.thumbnailWidth},\nthumbnailHeight: {self.thumbnailHeight},\nvideoCount: {self.videoCount}'
+
+        
 class youtubeConnect():
 
     def __init__(self,key):
@@ -94,17 +98,22 @@ class youtubeConnect():
 
 
     def video_by_url(self, url):
-        video = self.api.videos().list(part="snippet",id=url).execute()['items'][0]
+        '''
+        get video by the video url
+        '''
+        try:
+            video = self.api.videos().list(part="snippet",id=url).execute()['items'][0]
+        except:
+            print(f'the video with the id {url} is unavailible perhaps its private')
+            return False
         return videoobject(video)
 
 
-    def video_search(self,querry):
-        data = self.api.search().list(
-            part = "snippet",
-            order = "relevance",
-            q = querry,
-            type = "video"
-         )
+    def video_search(self,querry,limit=3):
+        '''
+        returns a list of videos that match querry on youtube
+        '''
+        data = self.api.search().list(part = "snippet",order = "relevance",q = querry,type = "video",maxResults=limit)
         li = []
         for videoData in data.execute()['items']:
             videoUrl = videoData['id']['videoId']
@@ -113,16 +122,16 @@ class youtubeConnect():
         return li
 
 
-    def playlist_search(self,querry):
+    def playlist_search(self,querry, limit=1,maxVidsInPL=20):
         '''
-        returns a list of playlists based off the querry
+        returns a list of playlists that match querry on youtube
         '''
-        request = self.api.search().list(part="snippet",q="never ending story",type="playlist")
+        request = self.api.search().list(part="snippet",q=querry,type="playlist",maxResults=limit)
         response = request.execute()
-        playists = []
+        playlists = []
         for playlist in response['items']:
-            playists.append(playlistobject(playlist))
-        return playists
+            playlists.append(playlistobject(playlist,maxVideos=maxVidsInPL))
+        return playlists
 
 
 if __name__ == "__main__":
