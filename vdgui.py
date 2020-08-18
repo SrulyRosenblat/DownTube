8f051ac2e3c8e40e14f40f27eb6451b553401fca
sruly <srulsterer@gmail.com>
2020-08-18 18:10:01 -0400

    got most of the layout to do basic functions (back and next buttons, search bar,description box...)

--- vdgui.py
+++ vdgui.py
@@ -3,22 +3,28 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import * 
 import sys
 import functools
-
+import ytsearch
+import key
+from urllib.request import urlretrieve
+import time
 class videoDownloaderGui(QMainWindow):
     def __init__(self, parent=None):
         '''
         sets up videoDownloader gui
         '''
         super().__init__(parent)
-        
         self.setGeometry(100, 100, 2000, 1250)
         self.setWindowTitle('youtube downloader')
         central = QWidget()
         self.setCentralWidget(central)
-        self.grid = QGridLayout()
-        self.grid.setAlignment(Qt.AlignCenter)
         self.statusBar = QStatusBar()
         self.setStatusBar(self.statusBar)
+        self.api = ytsearch.youtubeConnect(key.key)
+        self.index = 0
+        self.videos = []
+        self.grid = QGridLayout()
+        self.grid.setAlignment(Qt.AlignCenter)
+        
         self.setupGui()
         central.setLayout(self.grid)
         
@@ -40,69 +46,116 @@ class videoDownloaderGui(QMainWindow):
        # slider.sliderMoved(lambda:slider.setCursor(QCursor(Qt.ClosedHandCursor)))
         #slider.sliderMoved(lambda:slider.setCursor(QCursor(Qt.OpenHandCursor)))
 
-        description = QTextBrowser()
-        description.setText('rtgkeirjguermjmetuijmgiu\njguiwrhnuignwuhnuhn\nuj4wtjiu4hju8qh8h')
-        description.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding))
-        description.setStatusTip('the description of the current video')
+        self.description = QTextBrowser()
+        self.description.setText('rtgkeirjguermjmetuijmgiu\njguiwrhnuignwuhnuhn\nuj4wtjiu4hju8qh8h')
+        self.description.setSizePolicy(QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding))
+        self.description.setStatusTip('the description of the current video')
 
-        searchbar = QLineEdit()
-        searchbar.setPlaceholderText('search for video or enter url')
-        searchbar.setStyleSheet("border: 2px solid black;border-radius: 10px;font-size: 36px;")
-        searchbar.setSizePolicy(QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred))
-        searchbar.setStatusTip('search for a video')
+        self.searchbar = QLineEdit()
+        self.searchbar.setPlaceholderText('search for video or enter url')
+        self.searchbar.setStyleSheet("border: 2px solid black;border-radius: 10px;font-size: 36px;")
+        self.searchbar.setSizePolicy(QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred))
+        self.searchbar.setStatusTip('search for a video')
+        self.searchbar.returnPressed.connect(self.searchVids)
     
 
-        downloadbtn = QPushButton()
-        downloadbtn.setText('Download')
-        downloadbtn.setStyleSheet('background-color: rgb(0, 85, 255); border: 1px solid black;border-radius: 10px;font-size: 30px;color: white;padding: 5px 15px;')
-        downloadbtn.setSizePolicy(QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred))
-        downloadbtn.setCursor(QCursor(Qt.PointingHandCursor))
-        downloadbtn.setStatusTip('download the current video')
+        self.downloadbtn = QPushButton()
+        self.downloadbtn.setText('Download')
+        self.downloadbtn.setStyleSheet('QPushButton {background-color: rgb(0, 85, 255); border: 1px solid black;border-radius: 10px;font-size: 30px;color: white;padding: 5px 15px;}''QPushButton:pressed { background-color: gray }''QPushButton:hover { background-color: skyblue }')#''QPushButton:pressed { background-color: gray }'
+ 
+        self.downloadbtn.setSizePolicy(QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred))
+        self.downloadbtn.setCursor(QCursor(Qt.PointingHandCursor))
+        self.downloadbtn.setStatusTip('download the current video')
 
-        backbtn = QPushButton()
-        backbtn.setText('<<<')
-        backbtn.setSizePolicy(QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred))
-        backbtn.setCursor(QCursor(Qt.PointingHandCursor))
-        backbtn.setStatusTip('go to last video')
+        self.backbtn = QPushButton()
+        self.backbtn.setText('<<<')
+        self.backbtn.setSizePolicy(QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Preferred))
+        self.backbtn.setCursor(QCursor(Qt.PointingHandCursor))
+        self.backbtn.clicked.connect(lambda:self.changeIndex(-1))
+        self.backbtn.setStatusTip('go to last video')
+        self.backbtn.setEnabled(False)
         
-        nextbtn = QPushButton()
-        nextbtn.setText('>>>')
-        nextbtn.setSizePolicy(QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred))
-        nextbtn.setCursor(QCursor(Qt.PointingHandCursor))
-        nextbtn.setStatusTip('go to next video')
+        self.nextbtn = QPushButton()
+        self.nextbtn.setText('>>>')
+        self.nextbtn.setSizePolicy(QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Preferred))
+        self.nextbtn.setCursor(QCursor(Qt.PointingHandCursor))
+        self.nextbtn.clicked.connect(lambda:self.changeIndex(1))
+        self.nextbtn.setStatusTip('go to next video')
+        self.nextbtn.setEnabled(False)
+        
 
-        picture = QLabel()
-        img = QPixmap('./gui/thumbnail.jpg')
-        img = img.scaled(1280,720, Qt.KeepAspectRatio)
-        picture.setPixmap(img)
-        picture.setStatusTip('the thumbnail of the video selected')
+        self.picture = QLabel()
+        img = QPixmap('./gui/youtube-logo.jpg')
+        img = img.scaled(1280,720)
+        self.picture.setPixmap(img)
+        #self.picture.setSizePolicy(QSizePolicy.Fixed,QSizePolicy.Fixed)
+        self.picture.setStatusTip('the thumbnail of the video selected')
 
-        title = QLabel()
-        title.setText('oppo finally did it')
-        title.setStatusTip('the title of the selected video')
+        self.titleDisplay = QLabel()
+        self.titleDisplay
+        self.titleDisplay.setText('oppo finally did it')
+        self.titleDisplay.setStatusTip('the title of the selected video')
 
-        videoType = QComboBox()
-        videoType.addItems(['video and audio', 'audio only'])
-        videoType.setCursor(QCursor(Qt.PointingHandCursor))
-        videoType.setStatusTip('pick your download format')
+        self.videoType = QComboBox()
+        self.videoType.addItems(['video and audio', 'audio only'])
+        self.videoType.setCursor(QCursor(Qt.PointingHandCursor))
+        self.videoType.setStatusTip('pick your download format')
 
         searchbarSpacer = QSpacerItem(20, 60, QSizePolicy.Minimum, QSizePolicy.Maximum)
         descriptionSpacer = QSpacerItem(40, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
         downloadSpacer = QSpacerItem(20, 30, QSizePolicy.Maximum, QSizePolicy.Minimum)
 
-        self.grid.addWidget(videoType,3,9,1,2)
-        self.grid.addWidget(title,3,5,1,3)
-        self.grid.addWidget(picture,4,5,3,6)
-        self.grid.addWidget(backbtn,7,5,1,3)
-        self.grid.addWidget(nextbtn,7,8,1,3)
+        self.grid.addWidget(self.videoType,3,9,1,2)
+        self.grid.addWidget(self.titleDisplay,3,5,1,3)
+        self.grid.addWidget(self.picture,4,5,3,6)
+        self.grid.addWidget(self.backbtn,7,5,1,3)
+        self.grid.addWidget(self.nextbtn,7,8,1,3)
         self.grid.addItem(downloadSpacer,8,6,1,0)
-        self.grid.addWidget(downloadbtn,9,5,1,6)
+        self.grid.addWidget(self.downloadbtn,9,5,1,6)
         self.grid.addItem(searchbarSpacer,10,4,1,0)
-        self.grid.addWidget(searchbar,11,3,1,11)
+        self.grid.addWidget(self.searchbar,11,3,1,11)
         self.grid.addItem(descriptionSpacer,3,11,0,3)
-        self.grid.addWidget(description,3,14,4,3)
+        self.grid.addWidget(self.description,3,14,4,3)
         self.grid.addWidget(slider,4,0,3,4)
+    def searchVids(self):
+        self.videos = self.api.video_search(self.searchbar.text(),limit=5,thumbnailQuality='maxres',defultQuality='high')
+        self.downloadThumbnails()
+        self.setVideo()
+        if not self.index + 1 >= len(self.videos):
+            self.nextbtn.setEnabled(True)
 
+    def downloadThumbnails(self):        
+        count = 0
+        for vid in self.videos:
+            urlretrieve(vid.thumbnailLink, f'gui/thumbnails/thumbnail{count}.jpg' )
+            count += 1
+       
+    def setVideo(self):
+        img = QPixmap(f'gui/thumbnails/thumbnail{self.index}.jpg')
+        img = img.scaled(1280,720,transformMode=Qt.FastTransformation)
+        self.picture.setPixmap(img)
+        self.description.setText(self.videos[self.index].description)
+        self.setTitleText(self.videos[self.index].title)
+
+    def setTitleText(self,text):
+        if len(text) >= 40:
+            self.titleDisplay.setText(text[:40] + '...')
+        else:
+            self.titleDisplay.setText(text)
+
+    def changeIndex(self,num):
+        self.index += num
+        if self.index + 1 == len(self.videos):
+            self.nextbtn.setEnabled(False)
+            self.backbtn.setEnabled(True)
+        elif self.index == 0:
+            self.backbtn.setEnabled(False)
+            self.nextbtn.setEnabled(True)
+        else:
+            self.backbtn.setEnabled(True)
+            self.nextbtn.setEnabled(True)
+        self.setVideo()
+        
 app = QApplication(sys.argv)
 window = videoDownloaderGui()
 window.show()
