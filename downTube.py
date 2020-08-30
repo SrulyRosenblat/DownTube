16d8d7b3b4176b53451b5ead844a9a96b26c5b7e
sruly <srulsterer@gmail.com>
2020-08-30 00:45:37 -0400

    made downtube.py call vdgui.py

--- downTube.py
+++ downTube.py
@@ -1,23 +1,11 @@
-from pytube import YouTube
-import tubered as tr
-# from PIL import ImageTk,Image
-# import urllib.request
-# import time
-# import threading
-# import re
-# from moviepy.editor import *
+import sys
+from PyQt5.QtWidgets import QApplication
+from vdgui import videoDownloaderGui
 def main():
-    vids = tr.handshake_link('what does the fox say')
-    if type(vids) == list:
-        num = 0
-        for vid in vids:
-            print(vid.title)
-            vid.title = f'bob{num}'
-            print(vid.title)
-            num += 1
-    else:
-        print(vids.title)
-        
+    app = QApplication(sys.argv)
+    window = videoDownloaderGui()
+    window.show()
+    sys.exit(app.exec())
     return
 
 if __name__ == "__main__":              
